41479dbad470dd46cf2e655a71921af8affee535
João Viegas <joao.viegas@rummo.com>
2024-03-13 17:27:28 -0300

    feat: add list unique brands function

--- main.go
+++ main.go
@@ -92,3 +92,26 @@ func getCarsByBrand(brand string, cars []Car) []Car {
 
 	return filteredCars
 }
+
+func getUniqueBrands(cars []Car) []string {
+	uniqueBrand := make(map[string]bool)
+	var uniqueBrandsList []string
+
+	for _, car := range cars {
+		if _, found := uniqueBrand[car.Brand]; !found {
+			uniqueBrand[car.Brand] = true
+			uniqueBrandsList = append(uniqueBrandsList, car.Brand)
+		}
+	}
+
+	return uniqueBrandsList
+}
+
+func listUniqueBrands(cars []Car) {
+	uniqueBrands := getUniqueBrands(cars)
+
+	fmt.Println("Unique brands: ")
+	for _, uniqueBrand := range uniqueBrands {
+		fmt.Println(uniqueBrand)
+	}
+}
